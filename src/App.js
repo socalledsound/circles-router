8df3a49d3a680252c85637e703a5e838c876b50c
chris kubick <lrs6000@gmail.com>
2021-09-03 10:54:33 -0700

    starter

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import Header from './Header'
-import NewHomePage from './NewHomePage'
-// import CirclesPageParams from './CirclesPageParams' 
-import CirclesPage from './CirclesPage'
-import AboutPage from './AboutPage'
 import styles from './App.module.css'
 
-
 const App = () => {
-
     return ( 
-
         <div className={styles.bg}>
-        <Header />
-
-        <Switch>
-            <Route exact path='/' component={NewHomePage} />
-            <Route path = '/circles' component={CirclesPage} />
-            {/* <Route path='/circles/:id' component={CirclesPageParams}/> */}
-            <Route path='/about' component={AboutPage}/>
-            <Redirect to='/' />
-        </Switch>
-    
-        
-       
-     
+            hi there
         </div>
      );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import 'normalize.css'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <App />,
     document.getElementById('root')
 )
